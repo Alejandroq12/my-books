9176a34d75621da0c950a78128094046e9cebf37
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-22 18:05:56 -0600

    Add validation with js

--- index.js
+++ index.js
@@ -60,19 +60,44 @@ document.addEventListener('DOMContentLoaded', () => {
   const bookList = new BookList();
   const booksListUI = new BooksListUI(bookList);
 
+  function isValidInput(input) {
+    if (input.trim() === '') return false;
+    const regex = /^[a-zA-Z0-9\s]+$/;
+    return regex.test(input);
+  }
+
   document.getElementById('addBookButton').addEventListener('click', (event) => {
     event.preventDefault();
 
     const bookTitleInput = document.getElementById('bookTitle');
     const authorNameInput = document.getElementById('bookAuthor');
-    const bookTitle = document.getElementById('bookTitle').value;
-    const authorName = document.getElementById('bookAuthor').value;
+    const bookTitle = bookTitleInput.value;
+    const authorName = authorNameInput.value;
+
+    const errorMessage = document.getElementById('error-message');
+    const successMessage = document.getElementById('success-message');
+    errorMessage.textContent = '';
+    successMessage.textContent = '';
+
+    if (!isValidInput(bookTitle) || !isValidInput(authorName)) {
+      errorMessage.textContent = 'Please enter a valid title and author name (letters and numbers only).';
+      setTimeout(() => {
+        errorMessage.textContent = '';
+      }, 2000);
+      return;
+    }
+
     const newBook = new Book(bookTitle, authorName);
     bookList.addBook(newBook);
     booksListUI.render();
     // Clear the input fields after adding the book
     bookTitleInput.value = '';
     authorNameInput.value = '';
+
+    successMessage.textContent = 'Book successfully created.';
+    setTimeout(() => {
+      successMessage.textContent = '';
+    }, 2000);
   });
 
   booksListUI.render();
